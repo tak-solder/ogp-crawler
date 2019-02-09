e617202f91a0370555660504f4a679426a5b593c
tak-solder <tak.solder@gmail.com>
2019-02-09 21:09:22 +0900

    Lambdaプロキシ統合の形に修正

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const url = require("url");
 const request = require('request-promise');
 const cheerio = require('cheerio');
 const Parser = require('./Parser');
 
-module.exports.handler = async (req) => {
-    if (!req.queryStringParameters.url || !/^https?:\/\/([\w-]+\.)*[\w-]+(\/.*)?$/.test(req.queryStringParameters.url)) {
-        return {
-            status: 422,
-            error: 'URL wrong format',
-        };
+module.exports.handler = async (event, context, callback) => {
+    if (!event.queryStringParameters.url || !/^https?:\/\/([\w-]+\.)*[\w-]+(\/.*)?$/.test(event.queryStringParameters.url)) {
+        callback(null, {
+            "statusCode": 422,
+            "headers": {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            "body": JSON.stringify({
+                status: 422,
+                error: 'URL wrong format',
+            }),
+            "isBase64Encoded": false,
+        });
+        return;
     }
 
     const options = {
-        uri: req.queryStringParameters.url,
+        uri: event.queryStringParameters.url,
         timeout: 5000,
         transform: (body) => new Parser(cheerio.load(body)),
     };
@@ -22,16 +29,32 @@ module.exports.handler = async (req) => {
     });
 
     if (response.error) {
-        return response;
+        callback(null, {
+            "statusCode": response.status,
+            "headers": {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            "body": JSON.stringify(response),
+            "isBase64Encoded": false,
+        });
+        return;
     }
 
-    return {
-        status: 200,
-        title: response.getTitle(),
-        url: response.getUrl() || url,
-        image: response.getImage(),
-        siteName: response.getSiteName(),
-        description: response.getDescription(),
-        isLargeCard: response.isLargeCard(),
-    };
+    callback(null, {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        "body": JSON.stringify({
+            status: 200,
+            error: null,
+            title: response.getTitle(),
+            url: response.getUrl() || event.queryStringParameters.url,
+            image: response.getImage(),
+            siteName: response.getSiteName(),
+            description: response.getDescription(),
+            isLargeCard: response.isLargeCard(),
+        }),
+        "isBase64Encoded": false,
+    });
 };
